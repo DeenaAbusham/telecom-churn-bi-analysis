6665bd24c86e74067eece0dca3a992d5fb20ce25
DeenaAbusham <102671942+DeenaAbusham@users.noreply.github.com>
2026-09-22 00:08:29 +0400

    Add files via upload

---
+++ telecom-churn-bi-analysis/02_sql/04_churn_by_internet_service.sql.sql
@@ -0,0 +1,43 @@
+-- ============================================
+-- TELECOM CUSTOMER CHURN BI PROJECT
+-- 04 - Churn by Internet Service
+-- ============================================
+
+SELECT
+    internet_service,
+
+    COUNT(*) AS total_customers,
+
+    SUM(
+        CASE
+            WHEN churn = 'Yes' THEN 1
+            ELSE 0
+        END
+    ) AS churned_customers,
+
+    ROUND(
+        100.0 *
+        SUM(
+            CASE
+                WHEN churn = 'Yes' THEN 1
+                ELSE 0
+            END
+        ) / COUNT(*),
+        2
+    ) AS churn_rate,
+
+    ROUND(
+        AVG(monthly_charges),
+        2
+    ) AS avg_monthly_charge,
+
+    ROUND(
+        SUM(monthly_charges),
+        2
+    ) AS total_monthly_revenue
+
+FROM telecom_churn_clean
+
+GROUP BY internet_service
+
+ORDER BY churn_rate DESC;
---
+++ telecom-churn-bi-analysis/02_sql/08_revenue_at_risk.sql.sql
@@ -0,0 +1,37 @@
+-- ============================================
+-- TELECOM CUSTOMER CHURN BI PROJECT
+-- 08 - Revenue at Risk
+-- ============================================
+
+SELECT
+    contract,
+
+    COUNT(*) AS churned_customers,
+
+    ROUND(
+        SUM(monthly_charges),
+        2
+    ) AS monthly_revenue_at_risk,
+
+    ROUND(
+        AVG(monthly_charges),
+        2
+    ) AS avg_monthly_charge_churned,
+
+    ROUND(
+        100.0 *
+        SUM(monthly_charges) /
+        (
+            SELECT SUM(monthly_charges)
+            FROM telecom_churn_clean
+        ),
+        2
+    ) AS percentage_of_total_revenue
+
+FROM telecom_churn_clean
+
+WHERE churn = 'Yes'
+
+GROUP BY contract
+
+ORDER BY monthly_revenue_at_risk DESC;
